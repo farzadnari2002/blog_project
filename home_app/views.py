e6a9349cd52bf6fc7233bb523b6f66454f1c43bf
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-01-31 21:47:17 +0330

    query exercise

--- home_app/views.py
+++ home_app/views.py
@@ -6,5 +6,7 @@ from blog_app.models import Article
 def home(request):
     articles = Article.objects.filter(status=True)
     # articles = Article.objects.all()
+    article = Article.objects.get(title="python")
+    # print(f"ala {article.body}")
 
     return render(request, 'home_app/index.html', context={'articles': articles})
