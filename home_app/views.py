069534cdbdc1ef7f8731bc01ed01f5ff9982c506
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-01-31 21:57:08 +0330

    queryset,filter exercise

--- home_app/views.py
+++ home_app/views.py
@@ -6,7 +6,9 @@ from blog_app.models import Article
 def home(request):
     articles = Article.objects.filter(status=True)
     # articles = Article.objects.all()
-    article = Article.objects.get(title="python")
+    # article = Article.objects.get(title="python")
     # print(f"ala {article.body}")
+    qtest = Article.objects.filter(title="python")
+    print(qtest)
 
     return render(request, 'home_app/index.html', context={'articles': articles})
