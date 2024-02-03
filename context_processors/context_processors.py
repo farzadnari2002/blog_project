cee33f2663f49db90c8ae7ab108e769f81fa51a6
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-02-03 14:16:39 +0330

    define recent articles context processor and use in pages have sidebar with add in context_precessors list in blog_project.settings

--- context_processors/context_processors.py
+++ context_processors/context_processors.py
@@ -1,8 +1,7 @@
 from blog_app.models import Article
-from django.shortcuts import render
 
 
 
 def recent_articles(request):
     recent_articles = Article.objects.order_by('-created')[0:3]
-    return render(request, )
+    return {'recent_articles':recent_articles}
--- home_app/views.py
+++ home_app/views.py
@@ -7,6 +7,5 @@ from django.urls import reverse
 def home(request):
     articles = Article.objects.filter(status=True)
     # articles = Article.objects.all()
-    recent_articles = Article.objects.all().order_by('-created')[0:3]
 
-    return render(request, 'home_app/index.html', context={'articles': articles, "recent_articles":recent_articles})
+    return render(request, 'home_app/index.html', context={'articles': articles})
