e8cdb90790f3bf1ac9c452b594fd3a1d1c9f6dc8
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-02-03 15:42:03 +0330

    categories context processor defined

--- context_processors/context_processors.py
+++ context_processors/context_processors.py
@@ -1,7 +1,17 @@
 from blog_app.models import Article
+from blog_app.models import Category
 
 
 
 def recent_articles(request):
     recent_articles = Article.objects.order_by('-created')[0:3]
+    
     return {'recent_articles':recent_articles}
+
+
+
+def categories(request):
+    categories = Category.objects.all()
+    
+    return {"categories":categories}
+    
