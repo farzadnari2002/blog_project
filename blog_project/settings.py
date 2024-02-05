a0252a8593ab1b8434ece842cb7c23ad89203b4d
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-02-05 22:46:24 +0330

    social_share name in installed apps edited

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'blog_app.apps.BlogAppConfig',
     'django_cleanup.apps.CleanupConfig',
     'django_render_partial',
-    'social_share',
+    'django_social_share',
 
 
 
