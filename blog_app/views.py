ea36b3b4bd5946bb2817ee72d12a219946f4178c
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-02-08 15:15:46 +0330

    article deatil view update for comment object create and csrf_token add in form and massage name change to body

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from .models import Article, Category
+from .models import Article, Category, Comment
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
 
 
 
+
 def article_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
+    if request.method == 'POST':
+        body = request.POST.get('body')
+        Comment.objects.create(article=article, user=request.user, body=body)
+        
+
     return render(request, 'blog_app/article_detail.html', context={'article':article})
 
 
