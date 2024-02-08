d02ed925151d448afa374d9c13c6e6c7a7a1e11f
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-02-08 16:13:18 +0330

    reply comment bug fixed in artile detail template

--- blog_app/views.py
+++ blog_app/views.py
@@ -10,7 +10,8 @@ def article_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
     if request.method == 'POST':
         body = request.POST.get('body')
-        Comment.objects.create(article=article, user=request.user, body=body)
+        parent = 5
+        Comment.objects.create(article=article, user=request.user, body=body, parent_id=parent)
         
 
     return render(request, 'blog_app/article_detail.html', context={'article':article})
