6e6facca97f081749d9446f281a09fc4c795c3ad
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-02-16 17:37:55 +0330

    message word updated to true word

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 
 class ContactUsForm(forms.Form):
     name = forms.CharField(label='your name', max_length=10)
-    massage = forms.CharField(label='your massage', max_length=10)
+    massage = forms.CharField(label='your message', max_length=10)
+    
+    
+    def clean(self):
+        name = self.cleaned_data.get('name')
+        message = self.cleaned_data.get('message')
+        if name == message:
+            raise ValidationError
