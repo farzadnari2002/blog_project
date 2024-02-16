df02df4e8105019033a39cb8ff469ee01130faac
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-02-16 16:41:10 +0330

    template contactus form errors update for exercise

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 
 class ContactUsForm(forms.Form):
-    name = forms.CharField(label='enter name', max_length=10)
+    name = forms.CharField(label='your name', max_length=10)
     massage = forms.CharField(label='your massage', max_length=10)
