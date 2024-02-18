2e1e0924921949fc7f412d0e659eb6c80c4e2f79
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-02-18 18:22:07 +0330

    widget_tweaks package test end

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -38,13 +38,13 @@ class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = '__all__'
-        # widgets = {
-        #     'title': forms.TextInput(attrs={
-        #         'class': 'form-control',
-        #     }),
-        #     'text': forms.Textarea(attrs={
-        #         'class': 'form-control',
-        #     }),
-        # }
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'text': forms.Textarea(attrs={
+                'class': 'form-control',
+            }),
+        }
         
         
