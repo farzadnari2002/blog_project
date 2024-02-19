f67e74de53343c4680e2b2b2f6fdaa147db8fba5
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-02-19 12:25:19 +0330

    login view updating end

--- accounts_app/forms.py
+++ accounts_app/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from django.contrib.auth import authenticate
+from django.core.exceptions import ValidationError
 
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'input100'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'input100'}))
+    
+    
+    def clean(self):
+        user = authenticate(username=self.cleaned_data.get('username'), password=self.cleaned_data.get('password'))
+        if user is None:
+            raise ValidationError('username or password is wrong', code='invalid_info')
+        
