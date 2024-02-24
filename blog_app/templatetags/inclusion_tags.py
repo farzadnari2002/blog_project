acabf7168d7cb81c09b5bb198f61a6bd8dfb63d9
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-02-24 20:12:46 +0330

    change inclusion tag name and html file updated and use in article list template for test

--- blog_app/templatetags/inclusion_tags.py
+++ blog_app/templatetags/inclusion_tags.py
@@ -4,6 +4,6 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('blog_app/test_for_inclusion.html')
-def show_hello(name):
+def say_hello(name):
     return {'name':name}
 
