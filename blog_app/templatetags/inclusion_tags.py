e26b59dde9e51c4d6b963bea383defaeeee5ece7
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-02-25 13:39:49 +0330

    use custom inclusion tag for recent_articles show in sidebar

--- blog_app/templatetags/inclusion_tags.py
+++ blog_app/templatetags/inclusion_tags.py
@@ -4,6 +4,6 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('blog_app/test_for_inclusion.html')
-def say_hello(name):
-    return {'name':name}
+def show_objects(queryset):
+    return {'objects':queryset}
 
