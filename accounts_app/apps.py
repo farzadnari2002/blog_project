891503940705b74d9cd5c9ca6dd0b498273041a1
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-03-09 15:01:19 +0330

    blog_app and accounts_app verbose_name changed

--- accounts_app/apps.py
+++ accounts_app/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class AccountsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts_app'
-    verbose_name = 'accounts'
+    verbose_name = 'بخش حساب های کاربری'
