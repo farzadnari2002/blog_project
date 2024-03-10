aeed1d192410fd4bd640c784d99ac8dc1e35236e
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-03-10 13:26:16 +0330

    ArticleAdmin some attributes change

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -4,7 +4,11 @@ from .models import *
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'writer', 'show_image')
+    list_display = ('title', 'writer', 'show_image', 'status',)
+    list_filter = ('status', 'category')
+    search_fields = ('title',)
+    list_editable = ('status',)
+
     
     
 admin.site.register(Category)
