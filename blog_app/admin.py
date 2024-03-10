cb50a323374f9f280eed1d4dc65df651165eb0ba
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-03-10 13:58:57 +0330

    create FilterByTitle and use in ArticleAdmin.list_filter

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,11 +1,28 @@
+from typing import Any
 from django.contrib import admin
 from .models import *
+
+
+class FilterByTitle(admin.SimpleListFilter):
+    title = 'کلیدهای پرتکرار'
+    parameter_name = 'title'
+    
+    def lookups(self, request, model_admin):
+        return (
+            ('django', 'DJANGO'),
+            ('yek', 'YEK'),
+        )
+        
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(title__icontains=self.value())
+                
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'writer', 'show_image', 'status',)
-    list_filter = ('status', 'category')
+    list_filter = ('status', 'category', FilterByTitle)
     search_fields = ('title',)
     list_editable = ('status',)
 
