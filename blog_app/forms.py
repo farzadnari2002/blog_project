76563c47b3e140ecc578a7cefac401104f4fdd4f
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-03-22 18:38:10 +0330

    blog_app.ContactUsForm deleted

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -2,37 +2,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from .models import Comment
 
-
-
-class ContactUsForm(forms.Form):
-    years = ['2020', '2021', '2023']
-    FAVORITE_COLORS_CHOICES = [
-    ("blue","Blue"),
-    ("green", "Green"),
-    ("black", "Black"),
-    ]
-    name = forms.CharField(label='your name', max_length=10)
-    message = forms.CharField(label='your message', max_length=10)
-    data = forms.DateField(label='your date', widget=forms.SelectDateWidget(years=years, attrs={'class':'form-control'}))
-    favorite_colors = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=FAVORITE_COLORS_CHOICES)
-    
-    
-    def clean(self):
-        name = self.cleaned_data.get('name')
-        message = self.cleaned_data.get('message')
-        if name == 'farzad':
-            self.add_error('name', 'fazad nari cant send message')
-        if name == message:
-            raise ValidationError('name and message are same', code='name_message_same')
-        
-        
-    def clean_name(self):
-        name = self.cleaned_data.get('name')
-        if ' ' in name:
-            raise ValidationError('no space in name',code='name_space_error')
-        return name + ' test'
-    
-            
+         
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
